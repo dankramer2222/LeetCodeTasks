2c191890243c8ce6683a6d4229feab8cb445f39a
Danila Albutov <55891799+dankramer2222@users.noreply.github.com>
2023-11-01 00:00:58 -0700

    Time: 99 ms (52.24%), Space: 15.4 MB (73.63%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,10 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        result = 0
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = 0
         for num in nums:
-            result ^= num
-        return result
+            xor^=num
+        return xor
